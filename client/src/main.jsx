f8ae0c0fa11d2c2c6e000f1b0356a4258572961a
researchproject16 <gracesam0319@gmail.com>
2026-01-09 14:33:32 +0800

    Add client app files (Vite + React)

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error('Root element with id="root" not found in client/index.html');
+}
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
